9cea135e7a1eaa69be773eff88b59c14b488450d
sahasg96 <sahasg96@gmail.com>
2017-02-24 19:19:11 +0530

    buttonpress() function description

--- Pani-Puri/Pani-Puri.ino
+++ Pani-Puri/Pani-Puri.ino
@@ -1,12 +1,14 @@
 #include <Servo.h>
 
-Servo Prick;
-  int CD = 1;
+  Servo Prick;
+  int CD = 1; 
   int prick = 9; // Servo Motor done.
   int Aloo = 7;  //DC motor with gear done.
   int Pani = 4; 
   int Button = 5; 
-
+  int p = 0 , a = 0, h = 0;  //function counters.
+  int button_count = 0;
+  
 void setup() {
   // put your setup code here, to run once:
   pinMode(CD, OUTPUT);
@@ -45,17 +47,52 @@ void Rotate()
   digitalWrite(CD,LOW);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-if (digitalRead(Button) == HIGH) // If button is pressed execute below code.
+void startup()
 {
   Rotate();
   Hole();
   Rotate();
   Stuffing();
+  Hole();
   Rotate();
   Valve();
+  Stuffing();
+  Hole();
   Rotate();
+  Stuffing();
+  Hole();
+  
+  p=2;
+  a=4;
+  h=5;
+  button_count++;
 }
 
+boolean buttonpress()
+{
+  if(Button.read())
+    return true;
+   else
+    return false;
+  
+}
+void loop() {
+if(buttonpress() && button_count == 1)
+{
+  startup();
+}
+
+if(buttonpress() && button_count<=6 && button_count >1)
+  {
+    if(p<=6)
+      Valve();
+    Rotate();
+    if(a<=6)
+      Stuffing();
+    if(h<=6)
+      Hole();
+  }
+if(button_count == 6)
+  button_count=0;
+  
 }
